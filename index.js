f6f37222766a732a5dbfbbc8a7204314e83cd165
Nitesh093 <54766765+Nitesh093@users.noreply.github.com>
2023-05-02 04:17:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use((req,res,next)=>{
 })
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello Worlds!')
 })
 app.use(express.json())
 app.use('/api',require("./Routes/CreateUser"))
